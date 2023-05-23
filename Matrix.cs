f796d9011e74bf2a1c690ebff87cd6d05d96c814
expecto <expeo@vk.com>
2023-05-23 23:12:04 +0800

    task2 matrix

---
+++ Matrix.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Graph_tasks
+{
+    public partial class Matrix : Form
+    {
+        public Matrix(int[,] M)
+        {
+            InitializeComponent();
+            
+            for(int i = 0; i < M.GetLength(0); i++)
+            {
+                for(int j = 0; j < M.GetLength(0); j++)
+                {
+                    label1.Text += IntToChar(M[i, j]);
+                }
+                label1.Text += '\n';
+            }
+            
+        }
+
+        private char IntToChar(int n)
+        {
+            return n == 1 ? '1' : '0';
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+
+
+
+    }
+}
--- task2.cs
+++ task2.cs
@@ -17,9 +17,35 @@ namespace Graph_tasks
         {
             InitializeComponent();
         }
-        
+
         private void button1_Click(object sender, EventArgs e)
         {
+            int n = Convert.ToInt32(textBox2.Text);
+            if (n < 0 || n > 20)
+            {
+                MessageBox.Show("Неверное значение", "Введите количество вершин от 1 до 20", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+            else
+            {
+                this.ClientSize = new System.Drawing.Size(950, 473);
+                this.close.Location = new System.Drawing.Point(925, 4);
+                int[,] Matrix = GetAdjacencyMatrix(n);
+                Matrix M = new Matrix(Matrix);
+                M.ShowDialog();
+            }
+        }
+        private int[,] GetAdjacencyMatrix(int n)
+        {
+            int[,] AdjacencyMatrix = new int[n,n];
+            Random rnd = new Random();
+            for(int i = 0; i < n; i++)
+            {
+                for(int j = 0; j < n; j++)
+                {
+                    AdjacencyMatrix[i, j] = rnd.Next(0, 1);
+                }
+            }
+            return AdjacencyMatrix;
 
         }
         private void pictureBox1_Paint(object sender, EventArgs e)
