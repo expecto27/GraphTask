f6d4710a692fee4e0d1d7a0b572f9baea4cd0e10
expecto <expeo@vk.com>
2023-05-25 18:25:27 +0800

    task5 solution

--- task3.Designer.cs
+++ task3.Designer.cs
@@ -152,6 +152,7 @@
             // 
             // richTextBox1
             // 
+            this.richTextBox1.Font = new System.Drawing.Font("Impact", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
             this.richTextBox1.Location = new System.Drawing.Point(367, 25);
             this.richTextBox1.Name = "richTextBox1";
             this.richTextBox1.Size = new System.Drawing.Size(174, 79);
